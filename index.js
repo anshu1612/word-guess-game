5ccfd5e99e6a0a5ba96ec930b2668d19342d9484
anshu1612 <anshuchakrawarti26@gmail.com>
2025-01-12 20:31:22 +0530

    created win and game over modal

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const words = ["APPLE", "GRAPE", "MANGO", "PEACH", "GUAVA"];
 const missingIndex = [0, 2, 4];
 const boxes = document.querySelectorAll(".box");
-const btn = document.getElementById("btn");
+const okBtn = document.getElementById("okBtn");
 let wrong = document.getElementById("wrongGuess");
 let lifeline = document.getElementById("lifeline");
+const gameOver = document.getElementById("gameOver");
+const win = document.getElementById("win");
+const closeBtn = document.getElementById("closeBtn");
+const winCloseBtn = document.getElementById("closeBtn2");
 
 let wordIndex = Math.floor(Math.random() * 5);
 let word = words[wordIndex];
@@ -27,18 +31,47 @@ boxes.forEach((box, i) => {
 });
 
 let i = 0;
-btn.addEventListener("click", () => {
+okBtn.addEventListener("click", () => {
   let ans = document.getElementById("letter").value.toUpperCase();
-  if (lifeline.textContent > 0) {
-    if (ans == charArray[missingIndex[i]] && i<missingIndex.length) {
+  let lives = lifeline.textContent;
+  if (lives > 0) {
+    if (ans == charArray[missingIndex[i]] && i < missingIndex.length) {
       boxes[missingIndex[i]].textContent = ans;
       i += 1;
+      console.log(i);
+      if (i === missingIndex.length) {
+        console.log("win");
+        win.classList.remove("hidden");
+        document.body.classList.add("overflow-hidden");
+        document.getElementById("background").classList.add("blur");
+        okBtn.disabled = true;
+      }
     } else {
       lifeline.textContent = Number(lifeline.textContent) - 1;
       wrong.textContent = wrong.textContent + " " + ans;
     }
   }
-  else{
-    
+  if (lives <= 1) {
+    gameOver.classList.remove("hidden");
+    document.body.classList.add("overflow-hidden");
+    document.getElementById("background").classList.add("blur");
+    okBtn.disabled = true;
   }
+  document.getElementById("letter").value = "";
+});
+
+closeBtn.addEventListener("click", () => {
+  gameOver.classList.add("hidden");
+  document.body.classList.remove("overflow-hidden");
+  document.getElementById("background").classList.remove("blur");
+  okBtn.disabled = false;
+  location.reload();
+});
+
+winCloseBtn.addEventListener("click", () => {
+  win.classList.add("hidden");
+  document.body.classList.remove("overflow-hidden");
+  document.getElementById("background").classList.remove("blur");
+  okBtn.disabled = false;
+  location.reload();
 });
